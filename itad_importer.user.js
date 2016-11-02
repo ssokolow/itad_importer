84f1583cbf514079299c0eac4be71fdf9a23120a
Stephan Sokolow <http://www.ssokolow.com/ContactMe>
2016-11-02 06:41:39 -0400

    Bump version number to ensure updates get distributed

--- itad_importer.user.js
+++ itad_importer.user.js
@@ -3,7 +3,7 @@
 /* IsThereAnyDeal.com Collection Importer
 // ==UserScript==
 // @name IsThereAnyDeal.com Collection Importer
-// @version 0.1b14
+// @version 0.1b15
 // @namespace http://isthereanydeal.com/
 // @description Adds buttons to various sites to export your game lists to ITAD
 // @icon http://s3-eu-west-1.amazonaws.com/itad/images/banners/50x50.gif
